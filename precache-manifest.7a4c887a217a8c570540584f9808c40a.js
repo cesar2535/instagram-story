4ded023ab762db4e9cfdc32c7e6564eacb221a36
Cesar Chen <cesar6134@gmail.com>
2019-05-14 14:54:49 +0800

    Updates

---
+++ precache-manifest.7a4c887a217a8c570540584f9808c40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f09a2a9390a4248f980e22a5efcdbd",
+    "url": "/instagram-story/index.html"
+  },
+  {
+    "revision": "81c19a22b00b752538bf",
+    "url": "/instagram-story/static/css/main.ae1fa115.chunk.css"
+  },
+  {
+    "revision": "730eebfaf7fabb1a677b",
+    "url": "/instagram-story/static/js/2.324de25d.chunk.js"
+  },
+  {
+    "revision": "81c19a22b00b752538bf",
+    "url": "/instagram-story/static/js/main.f2ed7d19.chunk.js"
+  },
+  {
+    "revision": "2b5e123ce2a9ad8c01b4",
+    "url": "/instagram-story/static/js/runtime~main.0014d574.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instagram-story/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/instagram-story/precache-manifest.7a4c887a217a8c570540584f9808c40a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instagram-story/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
