62f3f2b430b84cb31529c8edf221dda036e5f436
Cesar Chen <cesar6134@gmail.com>
2019-05-17 18:52:36 +0800

    refactor(App.js): remove unused code
    remove the logo import

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { map, range } from 'ramda';
-import logo from './logo.svg';
 import './App.css';
 
 import StoryTransformer from '../components/StoryTransformer';
