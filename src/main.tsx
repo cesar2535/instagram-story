42ff064309365bc4fc2915260543ef6287017e66
Cesar Chen <cesar6134@gmail.com>
2024-03-20 22:39:52 +0800

    feat: setup vite configuration

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+async function main(element: Element | DocumentFragment) {}
+
+main(document.getElementById("root")!);
---
+++ src/vite-env.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="vites/client" />
