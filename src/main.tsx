9808e8b1f56287fb6bda40cff75bf45749897f12
Cesar Chen <cesar6134@gmail.com>
2025-05-26 16:35:44 +0800

    feat($package): add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import "./global.css";
 import "./index.css";
 
 async function main(element: Element | DocumentFragment) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
